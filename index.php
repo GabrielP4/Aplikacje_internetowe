474c988aa5fd94591216c97bd1d8e5669edb2ef1
GabrielP4 <gabriel.porczak@gmail.com>
2020-07-06 18:06:43 +0200

    PHP

--- index.php
+++ index.php
@@ -22,8 +22,8 @@
 
         </div>
         <ul>
-            <li><a href="service.html">Serwis</a></li>
-            <li><a href="pricelist.html">Cennik</a></li>
+            <li><a href="service.php">Serwis</a></li>
+            <li><a href="pricelist.php">Cennik</a></li>
             <li>
                 <div class="dropdown">
                     <a href="#id_main_contact" class="dropbtn">Kontakt</a>
@@ -121,36 +121,7 @@
             </div>
 
         </div>
-        <div class="down">
-
-            <div class="other">
-
-                <div class="social">
-                    <p class="header_other">Nasze sociale:</p>
-
-                    <a href="https://pl-pl.facebook.com/"> <i class="fab fa-facebook-square"></i></a>
-                    <a href="https://www.instagram.com/?hl=pl"><i class="fab fa-instagram-square"></i></a>
-                    <a href="https://twitter.com/explore"><i class="fab fa-twitter-square"></i></a>
-                </div>
-                <div class="contact">
-                    <p class="header_other">kontakt:</p>
-                    <p class="main_contact" id="id_main_contact">
-                        CompRes <br>
-                        tel. 123 456 789<br>
-                        www.compres.pl<br>
-                        email: biuro@compres.pl<br>
-                        Jarosław 1<br>
-                        37-500 Jarosław<br>
-                    </p>
-                </div>
-            </div>
-            <footer>
-
-                <p>© 2020 Gabriel P.</p>
-
-            </footer>
-
-        </div>
+        <?php include 'partial/footer.php';?>
 
     </main>
 
--- partial/footer.php
+++ partial/footer.php
@@ -1,30 +1,30 @@
 <div class="down">
 
-            <div class="other">
+    <div class="other">
 
-                <div class="social">
-                    <p class="header_other">Nasze sociale:</p>
+        <div class="social">
+            <p class="header_other">Nasze sociale:</p>
 
-                    <a href="https://pl-pl.facebook.com/"> <i class="fab fa-facebook-square"></i></a>
-                    <a href="https://www.instagram.com/?hl=pl"><i class="fab fa-instagram-square"></i></a>
-                    <a href="https://twitter.com/explore"><i class="fab fa-twitter-square"></i></a>
-                </div>
-                <div class="contact">
-                    <p class="header_other">kontakt:</p>
-                    <p class="main_contact" id="id_main_contact">
-                        CompRes <br>
-                        tel. 123 456 789<br>
-                        www.compres.pl<br>
-                        email: biuro@compres.pl<br>
-                        Jarosław 1<br>
-                        37-500 Jarosław<br>
-                    </p>
-                </div>
-            </div>
-            <footer>
-
-                <p>© 2020 Gabriel P.</p>
-
-            </footer>
-
+            <a href="https://pl-pl.facebook.com/"> <i class="fab fa-facebook-square"></i></a>
+            <a href="https://www.instagram.com/?hl=pl"><i class="fab fa-instagram-square"></i></a>
+            <a href="https://twitter.com/explore"><i class="fab fa-twitter-square"></i></a>
         </div>
+        <div class="contact">
+            <p class="header_other">kontakt:</p>
+            <p class="main_contact" id="id_main_contact">
+                CompRes <br>
+                tel. 123 456 789<br>
+                www.compres.pl<br>
+                email: biuro@compres.pl<br>
+                Jarosław 1<br>
+                37-500 Jarosław<br>
+            </p>
+        </div>
+    </div>
+    <footer>
+
+        <p>© 2020 Gabriel P.</p>
+
+    </footer>
+
+</div>
